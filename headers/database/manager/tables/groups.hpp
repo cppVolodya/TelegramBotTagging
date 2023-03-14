7d49fdbea00c6486e08565099a448dbebe9d361b
Volodymyr Dorozhovets <dorozovecvoloda@gmail.com>
2023-03-14 21:39:18 +0200

    added interface for database/manager/tables/Groups class

---
+++ headers/database/manager/tables/groups.hpp
@@ -0,0 +1,19 @@
+// Copyright [2023] <Volodymyr Dorozhovets>"
+
+#ifndef DATABASE_TABLES_GROUPS_HPP
+#define DATABASE_TABLES_GROUPS_HPP
+
+#include <string>
+#include <string_view>
+
+
+namespace N_TelegramBotTagging::N_Database::N_Tables
+{
+struct Groups
+{
+public:
+	int64_t m_id;
+};
+}  // namespace N_TelegramBotTagging::N_Database::N_Tables
+
+#endif  // DATABASE_TABLES_GROUPS_HPP
