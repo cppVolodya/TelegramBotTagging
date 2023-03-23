6f08835b0db692318b0626bc61aa533790313027
Volodymyr Dorozhovets <dorozovecvoloda@gmail.com>
2023-03-23 15:23:55 +0200

    added interface for ProgramCompletionStatus class

---
+++ headers/program_completion_status.hpp
@@ -0,0 +1,16 @@
+// Copyright [2023] <Volodymyr Dorozhovets>"
+
+#ifndef PROGRAM_COMPLETION_STATUS_HPP
+#define PROGRAM_COMPLETION_STATUS_HPP
+
+namespace N_TelegramBotTagging
+{
+enum class ProgramCompletionStatus
+{
+	success,
+	error,
+	critical
+};
+}  // namespace N_TelegramBotTagging
+
+#endif  // PROGRAM_COMPLETION_STATUS_HPP
